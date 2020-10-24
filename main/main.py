f4a2eabb7fbd28a9a1a71fadeaff83bece76f9b1
Eric Adler <tonsofpcs@gmail.com>
2020-10-24 19:44:44 -0400

    it's hard to concatenate when you're not told to!

--- main/main.py
+++ main/main.py
@@ -172,7 +172,7 @@ def act_temp():
     dbconn = sqlite3.connect(sqlite_database)
     db = dbconn.cursor()
 
-    sqlcommand = "INSERT INTO target_log (" + (int(time.time()) + "," target_temp + "," + targethigh + "," + targetlow + "," + hysteresis + ");"
+    sqlcommand = "INSERT INTO target_log (" + (int(time.time()) + "," + target_temp + "," + targethigh + "," + targetlow + "," + hysteresis + ");"
     db.execute(sqlcommand)
     db.execute("INSERT INTO temps (? , ?);", (int(time.time()), comp_temp) )
     db.commit()
