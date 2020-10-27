a77f33c8e63cf8ef2a98b54827ebbeb279ef9a4e
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2020-10-26 23:20:24 -0400

    status lookup

--- web/logdisplay.py
+++ web/logdisplay.py
@@ -106,7 +106,7 @@ for item in logdata_target:
   logdata_target_high_dates.append([mdates.date2num(datetime.fromtimestamp(item[0])), item[2]]) 
   logdata_target_low_dates.append([mdates.date2num(datetime.fromtimestamp(item[0])), item[3]])
 
-logdata_status_dates = [(mdates.date2num(datetime.fromtimestamp(item[0])), item[1]) for item in logdata_status]
+logdata_status_dates = [(mdates.date2num(datetime.fromtimestamp(item[0])), statuses[item[1]]) for item in logdata_status]
 
 #print "handling data"
 array_avg = np.array(logdata_avg_dates)
